1119a930cb6f8b90382cde891494a2f58f9c78a2
QuentinLamy <quentin.lamy16@gmail.com>
2023-10-31 16:54:32 +0100

    Update UIManager.cs

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class UIManager : Singleton
@@ -10,7 +11,19 @@ public class UIManager : Singleton
 
     //+ panels je suppose
 
+    public float Timer = 5f;
+
     public List<GameObject> existingObjects = new List<GameObject>();
+
+    public enum menuStates
+    {
+        MainMenu,
+        SkillTree,
+        Pause,
+        Settings,
+        InGame
+    }
+    public menuStates currentState;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +34,7 @@ public class UIManager : Singleton
     // Update is called once per frame
     void Update()
     {
-        /*if(Input.GetKeyDown(KeyCode.A)) {
-            Play();
-        }
-        if (Input.GetKeyDown(KeyCode.E)) {
-            MainMenu();
-        }*/
+
     }
 
     public void Play()
@@ -36,6 +44,7 @@ public class UIManager : Singleton
         }
         existingObjects.Clear();
         existingObjects.Add(Instantiate(InGameCanvas));
+        currentState = menuStates.InGame;
     }
 
     public void MainMenu()
@@ -45,6 +54,29 @@ public class UIManager : Singleton
         }
         existingObjects.Clear();
         existingObjects.Add(Instantiate(MainMenuCanvas));
+        currentState = menuStates.MainMenu;
+    }
+
+    public void SkillTree()
+    {
+        foreach (GameObject obj in existingObjects){
+            Destroy(obj);
+        }
+        existingObjects.Clear();
+        existingObjects.Add(Instantiate(SkillTreeCanvas));
+        currentState = menuStates.SkillTree;
+    }
+
+    public void PauseMenu()
+    {
+        existingObjects.Add(Instantiate(PauseCanvas));
+        currentState = menuStates.Pause;
+    }
+
+    public void Settings()
+    {
+        existingObjects.Add(Instantiate(PauseCanvas));
+        currentState = menuStates.Settings;
     }
 
 }
