e77b34f93a3214a315e9f0b9a28b5d3d6b25d0f3
QuentinLamy <quentin.lamy16@gmail.com>
2023-12-22 16:42:09 +0100

    alpha marche po

--- Assets/Scripts/FloatingText.cs
+++ Assets/Scripts/FloatingText.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     public Animator controller;
+    public AnimationCurve curve;
     float timer = 2f;
     public float n;
 
-    void Update()
+    private void Start()
     {
-        if(GameManager.Instance.currentState == GameManager.gameStates.Pause) return;
+        StartCoroutine(Play());
+    }
 
-        timer -= Time.deltaTime;
-        if(timer <= 0)
+    public IEnumerator Play()
+    {
+        float time = 0;
+        Color a = GetComponent<TextMesh>().color;
+        while (time < timer)
         {
-            Die();
+            while (GameManager.Instance.currentState == GameManager.gameStates.Pause) yield return null;
+            time += Time.deltaTime;
+            float strength = curve.Evaluate(time / timer);
+            a.a = strength;
+            GetComponent<TextMesh>().color = a;
+            yield return null;
         }
+        Die();
     }
 
     public void ResetTimer()
     {
-        timer = 2f;
+        StopAllCoroutines();
+        /*Color a = GetComponent<TextMesh>().color;
+        a.a = 1;
+        GetComponent<TextMesh>().color = a;*/
         controller.Play("FloatingText");
+        StartCoroutine(Play());
     }
 
     public void Die()
